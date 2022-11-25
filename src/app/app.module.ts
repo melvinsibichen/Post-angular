20476e58c06b1a28e6d3a4c447154590b845d587
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-25 15:39:40 +0530

    navabar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddpostComponent } from './addpost/addpost.component';
 import { ViewpostComponent } from './viewpost/viewpost.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddpostComponent,
-    ViewpostComponent
+    ViewpostComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
